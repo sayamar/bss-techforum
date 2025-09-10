94608b26603967fb7b233d1e5b7ef27df5623e6e
Maruthi SAYAMPU <maruthi.sayampu.ah@fujifilm.com>
2025-09-10 16:37:43 +0800

    Updated router at high level

--- src/App.js
+++ src/App.js
@@ -11,25 +11,39 @@ import BlogDetails from "./pages/BlogDetails/BlogDetails";
 
 export default function App({ toggleTheme, isDark }) {
   return (
-    <>
-     <Header toggleTheme={toggleTheme} isDark={isDark} />
-       {/* <div style={{ paddingTop: "70px" }}> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route
-          path="/blogs"
-          element={
-            <PrivateRoute>
-              <Blogs />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/createpost" element={<PrivateRoute><CreatePost /></PrivateRoute>} /> 
-        <Route path="/blogs/:id" element={<PrivateRoute><BlogDetails /></PrivateRoute>} />
-      </Routes>
-{/* </div> */}
-    </>
+    <Router>
+      <>
+        <Header toggleTheme={toggleTheme} isDark={isDark} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route
+            path="/blogs"
+            element={
+              <PrivateRoute>
+                <Blogs />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/createpost"
+            element={
+              <PrivateRoute>
+                <CreatePost />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/blogs/:id"
+            element={
+              <PrivateRoute>
+                <BlogDetails />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </>
+    </Router>
   );
 }
